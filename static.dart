ec87675752742650db7468179e0ec931b0541b03
Kshitij Verma <kshitijv250@gmail.com>
2022-12-10 22:45:07 +0530

    fixed static.dart

--- static.dart
+++ static.dart
@@ -1,17 +1,23 @@
-class Employee {
-  void display() {
-    print("I am working as an engineer");
+class Point {
+  int x;
+  int y;
+  static int count = 0;
+  Point([this.x = 0, this.y = 0]) {
+    count++;
   }
+
+  @override
+  String toString() => "($x,$y)";
+  int get countGet => count;
 }
 
-// Defining interface by implanting another class
-class Engineer implements Employee {
-  void display() {
-    print("I am an engineer in this company");
-  }
-}
+void main(List<String> args) {
+  Point p1 = Point();
+  Point p2 = Point(10, 20);
 
-void main() {
-  Engineer eng = new Engineer();
-  eng.display();
+  print(p1);
+  print(p2);
+  print("Count for p1 ${p1.countGet}");
+  print("Count for p2 ${p2.countGet}");
+  print("Count for point ${Point().countGet}");
 }
