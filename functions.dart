a3664398c70bc9097e4db48cf162390ce93f4b5d
Kshitij Verma <101321276+kshitijv256@users.noreply.github.com>
2022-12-18 23:08:59 +0530

    Update functions.dart

--- functions.dart
+++ functions.dart
@@ -4,8 +4,13 @@ int adder(int a, int b){
   return a+b;
 }
 
+double divider(int a, int b){
+  return a/b;
+}
+
 void main() {
   for (int i = 0; i < 5; i++) {
-    print('value ${adder(i,i+1)}');
+    print('add ${adder(i,i+1)}');
+    print('divide ${divider(i,i+1)}');
   }
 }
