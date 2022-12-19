34de6bc8c734c90189e1d40d4a000463e43819d5
Kshitij Verma <kshitijv250@gmail.com>
2022-12-19 21:38:45 +0530

    added fat arrow function

--- functions.dart
+++ functions.dart
@@ -1,16 +1,18 @@
-// A simple funtion to add two integers
+// Functions in Dart
 
-int adder(int a, int b){
-  return a+b;
+int adder(int a, int b) {
+  return a + b;
 }
 
-double divider(int a, int b){
-  return a/b;
+double divider(int a, int b) {
+  return a / b;
 }
+
+int adder2(int a, int b) => a + b;
 
 void main() {
   for (int i = 0; i < 5; i++) {
-    print('add ${adder(i,i+1)}');
-    print('divide ${divider(i,i+1)}');
+    print('add ${adder(i, i + 1)}');
+    print('divide ${divider(i, i + 1)}');
   }
 }
